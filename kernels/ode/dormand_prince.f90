2e1cd2eca07429adbb63cb4d86cd05367da6cdd9
Steve <mentalabuse2@gmail.com>
2025-12-21 19:27:19 +0000

    make subroutine pure

--- kernels/ode/dormand_prince.f90
+++ kernels/ode/dormand_prince.f90
@@ -117,7 +117,7 @@ module dormand_prince
     
     ! Abstract interface for ODE function
     abstract interface
-        subroutine ode_func_interface(t, y, dydt)
+        pure subroutine ode_func_interface(t, y, dydt)
             import :: wp
             real(wp), intent(in) :: t
             real(wp), intent(in) :: y(:)
